a9e139f7bf6344e3254f78b7a0891a5604135681
schktjm <si2.o.mjt@gmail.com>
2020-07-17 15:48:46 +0900

    :+1: ToDoListつくった

--- src/components/ToDos.tsx
+++ src/components/ToDos.tsx
@@ -17,33 +17,50 @@ const randomStr = (): string => {
 
 const ToDos: React.FC<Props> = props => {
     const [text, setText] = useState<string>("");
-    const [items, setItems] = useState<ItemType[]>([]);
+    const [items, setItems] = useState<{ [id: string]: ItemType }>({});
 
-    const setNewItem = (e: React.KeyboardEvent) => {
-        if (e.key == 'Enter') {
-            const i: ItemType = { text, done: false, id: randomStr() };
-            setItems([...items, i]);
-            setText("");
-        }
-    }
     const handleTextInput = (e: React.ChangeEvent) => {
         const target = e.target as HTMLInputElement;
         setText(target.value);
     }
+    const setNewItem = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            const item: ItemType = { text, done: false, id: randomStr() };
+            setItems({ ...items, [item.id]: item });
+            setText("");
+        }
+    }
+    const deleteItem = (id: string) => () => {
+        const { [id]: _, ...lefted } = items;
+        setItems(lefted)
+    }
+    const checkItem = (id: string) => () => {
+        const item = items[id];
+        item.done = !item.done;
+        setItems({ ...items, [id]: item });
+    }
 
     return (
         <Wrapper>
-            <TextInput type="text" value={text} onChange={handleTextInput} onKeyDown={setNewItem} />
+            <TextInput id="text-field" value={text} onChange={handleTextInput} onKeyDown={setNewItem} />
             <List>
-                {items.map(item => (<Item key={item.id}>{item.text}</Item>))}
+                {
+                    Object.values(items).map(item => (
+                        <Item id={item.id} key={item.id}>
+                            <CheckBox checked={item.done} onChange={checkItem(item.id)} /> {item.text} <DeleteBtn onClick={deleteItem(item.id)}>x</DeleteBtn>
+                        </Item>
+                    ))
+                }
             </List>
         </Wrapper>
     )
 };
 
 const Wrapper = styled.div``;
-const TextInput = styled.input``;
+const TextInput = styled.input.attrs({ type: "text" })``;
 const List = styled.ul``;
 const Item = styled.li``;
+const CheckBox = styled.input.attrs({ type: "checkbox" })``;
+const DeleteBtn = styled.button``;
 
 export default ToDos;
